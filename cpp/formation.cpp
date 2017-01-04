1c3e7b7951dbdd048c745786c84519a9b039a2c7
S0lidSanek <double-h@hotmail.fr>
2017-01-04 11:45:04 +0100

    Formation
    Ajoute classe formation

---
+++ cpp/formation.cpp
@@ -0,0 +1,51 @@
+/**
+	* @file formation.cpp
+	* @brief Definition des methodes de la classe formation
+	* @autor Hicham Chouhad
+	* @version 1.0
+*/
+
+#include "../lib/formation.h"
+#include "../lib/afficheurConsole.h"
+
+/**
+@brief Constructeur de l'objet formation
+@param[in] nomFormation - nom de la formation
+@param[in] nombreEtudiant - Nombre d'etudiant dans la formation
+*/
+formation::formation(const string& nomFormation, int nombreEtudiant) : ressource{nomFormation}, d_nombreEtudiant{nombreEtudiant}
+{}
+
+/**
+@brief Modifie le nombre d'etudiant dans la formation
+@param[in] nombreEtudiant - le nombre d'etudiant
+*/
+void formation::changerNombreEtudiant(int nombreEtudiant)
+{
+	d_nombreEtudiant = nombreEtudiant;
+}
+
+/**
+@brief renvoie le nombre d'etudiant dans la formation
+@return d_nombreEtudiant - le nombre d'etudiant
+*/
+int formation::nombreEtudiant()
+{
+	return d_nombreEtudiant;
+}
+
+/**
+@brief Redefinition de l'operateur <<
+@param[in, out] ost - flux de sortie
+@param[in] f - une formation
+*/
+std::ostream& operator<<(std::ostream& ost, const formation& f)
+{
+	/*
+	afficheurConsole aff;
+	formation * f2 = new formation(f);
+	aff.afficheFormation(f2, ost);
+	delete f2;
+	return ost;
+	*/
+}
--- lib/formation.h
+++ lib/formation.h
@@ -1,8 +1,8 @@
 /**
  * @file formation.h
  * @brief Classe permettant la gestion des formations
- * @author Adrien Couchot
- * @version 1.0
+ * @author Hicham Chouhad
+ * @version 1.1
  */
 
 #ifndef FORMATION_H
@@ -14,10 +14,19 @@ using std::string;
 
 class formation : public ressource{
 	public:
-		formation(const string & nom, int nombreDEtudiant);
-		int nombreDEtudiant() const;
+		formation(const string & nomFormation, int nombreEtudiant);
+		formation() = delete;
+		formation(const formation &f) = default;
+		
+		//SET
+		void changerNombreEtudiant(int nombreEtudiant);
+		
+		//GET
+		int nombreEtudiant() const;
+		
+		friend std::ostream& operator<<(std::ostream& ost, const formation& f);
 	private:
-		int d_nombreDEtudiant;
+		int d_nombreEtudiant;
 };
 
 #endif
